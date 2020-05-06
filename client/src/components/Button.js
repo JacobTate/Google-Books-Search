575af424c1fdb889f7e0f629653af84f416bc121
JacobTate <jacob87608@gmail.com>
2020-05-06 18:11:26 -0500

    thumbnails displaying search not working

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -1,33 +1,5 @@
 import React from "react";
-import axios from "axios";
-import Card from "./Card"
-import API from "../utils/API"
 function Button (props) {
- //   const axios = require("axios");
-function bookSearch(bookName){
-    // axios.get("/api/testget").then(res => {})
-    axios.get(`https://www.googleapis.com/books/v1/volumes?q=${bookName}&key=AIzaSyBsqVqaORZbJ82yW1hFcUcMHUB-ZEGexo8`)
-    .then(function (res) {
-        console.log(res.data.items);
-        
-        //  axios.post("/api/book/save", {
-        //      bookData: res.data.items
-        //  }).then(res => {
-        //      console.log(res); 
-        //  }).catch(err => {
-        //      console.log(err);
-        //  });
-    });
- 
-}
-// function renderCard () {
-//     return (
-//     <Card 
-//         imageLink={"https://image.shutterstock.com/image-photo/mountains-during-sunset-beautiful-natural-260nw-407021107.jpg"}
-//     />
-//     )
-// }
-
-return <button onClick={() => {props.renderCard()}}>click here</button>;
+return <button onClick={() => {props.isRendered()}}>click here</button>;
 }
 export default Button;
--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,53 +1,83 @@
 import React, {Component} from "react";
-//import axios from "axios";
+import axios from "axios";
 import Card from "./Card";
 import Button from "./Button"
+import Input from "./Input"
 class HomePage extends Component {
-    constructor () {
-        super()
+    constructor (props) {
+        super(props)
         this.state = {
             isRendered: false,
-            imgArr: ["https://image.shutterstock.com/image-photo/mountains-during-sunset-beautiful-natural-260nw-407021107.jpg", "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg"]
+            isLoaded: false,
+            bookName: "",
+            imgArr: [],
+            bookTitle: "",
+            author: "",
+            desc: "", 
         }
     }
-    //  bookSearch(bookName){
-    //     axios.get(`https://www.googleapis.com/books/v1/volumes?q=${bookName}&key=AIzaSyBsqVqaORZbJ82yW1hFcUcMHUB-ZEGexo8`)
-    //     .then(function (res) {
-    //         console.log(res.data.items);
-    //         return(
-    //         <Card 
-    //         imageLink={res.data.items[0].volumeInfo.thumbnail}
-    //         />
-    //         )
-    //     });
-    // }
+    handleInputChange = event => {
+        let value = event.target.value;
+        this.setState({
+          bookName: value
+        });
+        console.log(this.state.bookName);
+        
+      };
     isRendered () {
-       
-      
       this.state.isRendered ? this.setState({isRendered: false}): this.setState({isRendered: true});
-      return this.state.isRendered
+      return this.state.isRendered;
     }
-    renderCard () {
+    renderCard (imageArr) {
+        
+        if (imageArr.length === 0) {
+            return console.log(`404 no results found`);
+        }
+        else{
+            console.log(this.state);
         return(
             <div>
                 {
-      this.state.imgArr.map(item => (
-              <Card 
-              imageLink={item}
-              />     
+      imageArr.map(item => (
+           <Card 
+           imageLink={item.volumeInfo.imageLinks.thumbnail}
+           />
     ))
     }
     </div>
         )
+}
     }
+
+
+    componentDidMount () {
+    axios.get(`https://www.googleapis.com/books/v1/volumes?q=${this.state.bookName}&key=AIzaSyBsqVqaORZbJ82yW1hFcUcMHUB-ZEGexo8`)
+       .then(res => {  
+           let imageArr = res.data.items;
+           this.setState({
+                imgArr: imageArr
+           })
+                
+})
+.catch(err => console.log(err)); 
+}
     render() {
         return(
             <div>
             <Button 
-            renderCard={this.isRendered.bind(this)}
+            isRendered={this.isRendered.bind(this)}
             />
-            {this.state.isRendered ? this.renderCard(): null}
-            </div>
+            {this.state.isRendered ? this.renderCard(this.state.imgArr): null}
+            <form className="form">
+       <input
+            value={this.state.bookName}
+            name="searchBook"
+            onChange={this.handleInputChange}
+            type="text"
+            placeholder="Search"
+          />
+  </form>
+                        </div>
         )
     }
 };
